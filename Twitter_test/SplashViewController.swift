fdfc9b4852590ffa940a0aa441783f58cca86603
miuchena <ana.techon@gmail.com>
2018-01-11 23:00:48 +0300

    commit

---
+++ Twitter_test/SplashViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SplashViewController.swift
+//  Twitter_test
+//
+//  Created by Anastasia Krasnova on 15/11/2017.
+//  Copyright © 2017 Anastasia Krasnova. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
